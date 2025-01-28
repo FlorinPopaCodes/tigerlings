c1da422a8deed293f33b15255a5298dd66d9a13e
Florin Popa <456185+FlorinPopaCodes@users.noreply.github.com>
2025-01-28 22:49:46 +0200

    Solutions #2

--- exercises/032_rate_limiting_bandwidth.sh
+++ exercises/032_rate_limiting_bandwidth.sh
@@ -11,14 +11,16 @@ tb "create_accounts id=3200 code=10 ledger=320,
 # Instead of transferring an amount representing the number of requests, we'll transfer an amount representing the number of bytes.
 tb "create_transfers id=32000 debit_account_id=3200 credit_account_id=3201 amount=10000000 ledger=320 code=10;"
 
+TIMEOUT=1
 # Let's say that the user is sending requests that are 1 MB each.
 for ((i=1; i<=11; i++)); do
     id=$((32000 + i))
     # What should the amount be?
-    amount=???
-    tb "create_transfers id=${id} debit_account_id=3201 credit_account_id=3200 amount=${amount} timeout=60 ledger=320 code=10 flags=pending;"
+    sleep 1.2
+    amount=1000000
+    tb "create_transfers id=${id} debit_account_id=3201 credit_account_id=3200 amount=${amount} timeout=${TIMEOUT} ledger=320 code=10 flags=pending;"
 done
 
 # As before, if you want to test that the balance resets, you can uncomment the following lines to sleep for 60 seconds before creating the last transfer.
-# sleep 60
-# tb "create_transfers id=32012 debit_account_id=3201 credit_account_id=3200 amount=10000000 timeout=60 ledger=320 code=10 flags=pending;"
+sleep $TIMEOUT
+tb "create_transfers id=32012 debit_account_id=3201 credit_account_id=3200 amount=10000000 timeout=60 ledger=320 code=10 flags=pending;"
